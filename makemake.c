818dd6ec73da8b0cc16ebaee3ff66f4fe67ad3f1
Ntrancha <101010.nk@gmail.com>
2015-08-03 21:12:50 +0200

    second

--- makemake.c
+++ makemake.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   makemake.c                                         :+:      :+:    :+:   */
+/*   make_source.c                                       :+:      :+:    :+:  */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ntrancha <ntrancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/03 17:00:23 by ntrancha          #+#    #+#             */
-/*   Updated: 2015/08/03 18:06:09 by ntrancha         ###   ########.fr       */
+/*   Updated: 2015/08/03 21:07:31 by ntrancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ char        *get_header(char *file)
     return (content);
 }
 
-void        add_source(char *line)
+char        *source(char *line)
 {
     int     index;
     int     start;
     char    *file;
+    char    *source;
 
     index = 0;
     while (line[index] != ' ' && line[index] != '\t')
@@ -38,15 +39,16 @@ void        add_source(char *line)
     while (line[index] != '(')
         index++;
     file = ft_strsub(line, start, index - start);
-    ft_putendl(file);
+    source = ft_strjoin(file, ".c");
     ft_strdel(&file);
+    return (source);
 }
 
-void        add_header(char *file)
+void        add_header(char *file, t_list *list)
 {
     char    *content;
     char    *line;
-    char    *source;
+    char    *src;
     int     num;
 
     content = get_header(file);
@@ -55,31 +57,120 @@ void        add_header(char *file)
     while (line)
     {
         if (ft_strcchr(line, ");") != 0 && ft_strcchr(line, "ft_") != 0)
-            add_source(line);
+        {
+            src = source(line);
+            ft_listadd(list, (void*)src);
+        }
         ft_strdel(&line);
         line = ft_strgetline(content, num++);
         if (ft_strcmp(line, "#endif") == 0)
-        {
             ft_strdel(&line);
-            //line = NULL;
-        }
     }
     ft_strdel(&content);
+}
+
+void        col0(t_list *str, char *content, int size, int max)
+{
+    ft_listadd(str, ft_strdup("\t\t"));
+    ft_listadd(str, content);
+    while (size % 4 != 0)
+    {
+        ft_listadd(str, ft_strdup(" "));
+        size++;
+    }
+    while (size + 4 <= max)
+    {
+        ft_listadd(str, ft_strdup("\t"));
+        size += 4;
+    }
+}
+
+void        col1(t_list *str, char *content, int size, int max)
+{
+    ft_listadd(str, content);
+    while (size % 4 != 0)
+    {
+        ft_listadd(str, ft_strdup(" "));
+        size++;
+    }
+    while (size + 4 <= max)
+    {
+        ft_listadd(str, ft_strdup("\t"));
+        size += 4;
+    }
+    ft_listadd(str, ft_strdup("\\\n"));
+}
+
+
+int         add_source(t_node *node, t_list *str, int col, int max)
+{
+    int     size;
+    char    *content;
+
+    size = ft_strlen((char *)node->content);
+    col = (col) ? 0 : 1;
+    content = ft_strdup((char *)node->content);
+    if (col == 0)
+        col0(str, content, size, max);
+    else
+        col1(str, content, size, max);
+    return (col);
+}
+
+void        create(t_list *list)
+{
+    int     max;
+    t_node  *node;
+    t_list  *str;
+    char    *content;
+    int     col;
+
+    max = (((ft_liststrlenmax(list) / 4) + 1) * 4);
+    node = list->start;
+    col = 1;
+    str = ft_listcreate();
+    while (node)
+    {
+        col = add_source(node, str, col, max);
+        node = node->next;
+    }
+    content = ft_listtostr(str);
+    ft_putendl(content);
+    ft_strdel(&content);
+    ft_listdel(str, ft_memdel);
+}
+
+void        make_header(t_opt *options)
+{
+    char    *flag;
+    char    *name;
+
+    name = ft_optgetopt_double(options, "-n");
+    if (!name)
+        name = "libft.a";
+    flag = ft_optgetopt_double(options, "-f");
+    if (!flag)
+        flag = "libft.a";
 }
 
 int         main(int argc, char **argv)
 {
     t_opt   *files;
+    t_list  *list;
     char    *file;
 
     files = ft_optget(argc, argv);
+    make_header(files);
     file = ft_optgetopt_next(files);
+    list = ft_listcreate();
     while (file)
     {
-        add_header(file);
+        add_header(file, list);
         ft_strdel(&file);
         file = ft_optgetopt_next(files);
     }
+    create(list);
+    ft_listdel(list, ft_memdel);
     ft_optdel(files);
     return (1);
 }
